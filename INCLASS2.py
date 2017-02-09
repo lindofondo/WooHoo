0495b53a342f2117943dc63e1e78e71fdfc04a9c
lindofondo <lindofondo.a@gmail.com>
2017-02-08 19:07:51 -0500

    HEY, it works!

---
+++ INCLASS2.py
@@ -0,0 +1,9 @@
+a = float(input ("Hey, you, yeah you--enter a WHOLE number: "))
+b = (a % 2)
+if b == 0:
+    print ("You've got an even number!")
+elif b == 1:
+    print ("Your number is odd.")
+else:
+    print ("Are you touched or something? A WHOLE NUMBER I said! ")
+
